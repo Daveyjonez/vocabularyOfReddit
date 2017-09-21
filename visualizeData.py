45dbdedccf8a76a643e039999e28d790227a71c8
David Owens <davidowens088@gmail.com>
2017-09-21 12:48:46 -0700

    Restructuring visualizeData
    Started to define general functions for creation of generic plots for data collected

--- visualizeData.py
+++ visualizeData.py
@@ -9,9 +9,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats.stats import pearsonr
 
-
 DATA_DIR = 'output'
 plt.style.use('ggplot')
+
+allLabels = []
+allSubscriberCount = []
+allVocabSize = []
+
+def makePlots(dataCsv):
+    labels = []
+    subscriberCounts = []
+    vocabSizes = []
+    mostFrequent = []
+
+    # Get data from csv
+    with open(os.path.join(DATA_DIR, dataCsv), 'r') as csvFile:
+        r = csv.reader(csvFile, delimiter=',')
+        for row in r:
+            labels.append(row[0])
+            subscriberCounts.append(row[1])
+            vocabSizes.append(row[2])
+
+            allLabels.append(row[0])
+            allSubscriberCount.append(row[1])
+            allVocabSize.append(row[2])
+
+    # Do im
+    fig = plt.figure()
+    plt.scatter(subscriberCounts, vocabSizes, s = 15)
+    fig.suptitle(labels, fontsize = 12)
+    plt.suptitle('Relationship between subscriber count and vocab size for '
+                 + dataCsv[:-4] + '\n related subreddits')
+    plt.xlabel('Subscriber Count')
+    plt.ylabel('Vocabulary Size')
+    plt.show()
 
 
 ###############################################################################
@@ -22,51 +53,14 @@ if __name__ == '__main__':
     topics = os.listdir(DATA_DIR)
     topics.pop(0)
 
-    labels = []
-    allLabels = []
+    makePlots(topics[1])
 
-    subscriberCount = []
-    allSubscriberCount = []
-
-    vocabSize = []
-    allVocabSize = []
-
-    mostFrequent = []
-
-    for topic in topics:
-        with open(os.path.join(DATA_DIR, topic), 'r') as csvFile:
-            r = csv.reader(csvFile, delimiter=',')
-            for row in r:
-                allLabels.append(row[0])
-                allSubscriberCount.append(row[1])
-                allVocabSize.append(row[2])
-
-    # Save vocabulary data
-    with open(os.path.join(DATA_DIR, topics[1]), 'r') as csvFile:
-        r = csv.reader(csvFile, delimiter=',')
-        for row in r:
-            labels.append(row[0])
-            subscriberCount.append(row[1])
-            vocabSize.append(row[2])
-            mostFrequent.append(eval(row[3]))
-
-    subscriberCount = list(map(int, allSubscriberCount))
-    vocabSize = list(map(int, allVocabSize))
-
-    print(pearsonr(subscriberCount, vocabSize))
+    '''
+    allSubscriberCount = list(map(int, allSubscriberCount))
+    allVocabSize = list(map(int, allVocabSize))
 
     bestFit = np.polyfit(x = subscriberCount, y = vocabSize, deg=1)
     p = np.poly1d(bestFit)
     bestFitX = np.arange(min(subscriberCount), max(subscriberCount))
     bestFitY = p(bestFitX)
-
-    print(p)
-
-    fig = plt.figure()
-    plt.scatter(subscriberCount, vocabSize, s = 15)
-    plt.plot(bestFitX, bestFitY, color = 'k')
-    fig.suptitle('Relationship between subreddit size and vocabulary size',
-                 fontsize = 15)
-    plt.xlabel('Subscriber Count')
-    plt.ylabel('Vocabulary Size')
-    plt.show()
+    '''
